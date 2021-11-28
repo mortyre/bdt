6b025e5f21e729c955a3465fe53cbf68a439773d
Stanislav Starkov <s.starkov@corp.mail.ru>
2021-11-28 14:13:59 +0300

    add mock test for asset lib

--- python-course/asset/test_Starkov_Stanislav_asset_mock_usd_enclosing.py
+++ python-course/asset/test_Starkov_Stanislav_asset_mock_usd_enclosing.py
@@ -6,10 +6,16 @@ from asset import Asset
 @patch("cbr.get_usd_course")
 def test_can_mock_external_calls(mock_get_usd_course):
 ##code##
-    mock_get_usd_course.side_effect = [76.32, 77.44, ConnectionError]
+    iterations = [0, 1, 2, 3, 4]
+    #iterations = list(range(10))
+
+    def calculate_used_for_iteration() -> float:
+        return 76.32 + (0.1 * iterations.pop())
+    
+    mock_get_usd_course.side_effect = calculate_used_for_iteration
 
     asset_property = Asset(name="property", capital=10**6, interest=0.1)
-    for iteration in range(0, 2, 2):
+    for iteration in reversed(range(len(iterations))):
         expected_revenue = (76.32 + 0.1 * iteration) * asset_property.capital * asset_property.interest
         calculated_revenue = asset_property.calculate_revenue_from_usd(years=1)
         assert calculated_revenue == pytest.approx(expected_revenue, abs=0.01), (
--- python-course/mock/PY-MADE-2021-Q4_Starkov_Stanislav_HW05/task_Starkov_Stanislav_asset_mock_usd_enclosing.py
+++ python-course/mock/PY-MADE-2021-Q4_Starkov_Stanislav_HW05/task_Starkov_Stanislav_asset_mock_usd_enclosing.py
@@ -1,1 +1,4 @@
-mock_get_usd_course.side_effect = [76.32, 77.44, ConnectionError]
+  iterations = [0, 1, 2, 3, 4]
+  def calculate_used_for_iteration() -> float:
+      return 76.32 + (0.1 * iterations.pop())
+  mock_get_usd_course.side_effect = calculate_used_for_iteration
